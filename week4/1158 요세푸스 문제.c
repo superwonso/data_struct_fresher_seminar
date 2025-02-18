19f47bb0cfcb09a796ba79e1a0bd5e0e6cf53f77
yuvnn <yubin11890@gmail.com>
2025-02-18 13:56:06 +0900

    week4 1(way1)

--- week4/1158 요세푸스 문제.c
+++ week4/1158 요세푸스 문제.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 typedef struct node{
     int data;
-    node* link;
+    struct node* link;
 }node;
 
 node* insert(int d,node *pre){
     node* tmp = (node*)malloc(sizeof(node));
+    tmp->link = pre->link;
     pre->link = tmp;
     tmp->data = d;
-    tmp->link = pre;
     return tmp;
 }
 
+int delete(node *pre, node *now){
+    int data = now->data;
+    pre->link = now->link;
+    //free(now);
+    return data;
+}
 
 int main(){
     int n,k;
-    scanf("%d %d",n,k);
+    scanf("%d %d",&n,&k);
+
     node* head = (node*)malloc(sizeof(node));
     head -> data = 1;
     head -> link = head;
+
     node *pre = head;
     for(int i=2;i<n+1;i++){
         pre = insert(i,pre);
     }
-    
 
-
+    printf("<");
+    node *now = pre;
+    for(int cnt= 0;cnt<n;cnt++){
+        for (int i = 0;i<k;i++){
+            pre = now;
+            now = now->link;
+        }
+        printf("%d",delete(pre,now));
+        if (cnt < n - 1) {printf(", ");}
+    }
+    printf(">");
+    return 0;
 }
