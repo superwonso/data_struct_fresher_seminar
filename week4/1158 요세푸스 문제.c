1451108646b4bf4db5cf7776c4fab2b3c68b379f
yuvnn <yubin11890@gmail.com>
2025-02-19 11:45:36 +0900

    code simplification & del files

--- week4/1158 요세푸스 문제.c
+++ week4/1158 요세푸스 문제.c
@@ -1,47 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct node{
+
+typedef struct node {
     int data;
     struct node* link;
-}node;
+} node;
 
-node* insert(int d,node *pre){
+node* insert(int d, node* pre) {
     node* tmp = (node*)malloc(sizeof(node));
+    tmp->data = d;
     tmp->link = pre->link;
     pre->link = tmp;
-    tmp->data = d;
     return tmp;
 }
 
-node* delete(node *pre, node *now){
-    pre->link = now->link;
-    free(now);
-    return pre;
-}
-
-int main(){
-    int n,k;
-    scanf("%d %d",&n,&k);
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
 
     node* head = (node*)malloc(sizeof(node));
-    head -> data = 1;
-    head -> link = head;
+    head->data = 1;
+    head->link = head;
 
-    node *pre = head;
-    for(int i=2;i<n+1;i++){
-        pre = insert(i,pre);
+    node* tail = head;
+    for (int i = 2; i <= n; i++) {
+        tail = insert(i, tail);
     }
 
     printf("<");
-    node *now = pre;
-    for(int cnt= 0;cnt<n;cnt++){
-        for (int i = 0;i<k;i++){
+    node* pre = tail, *now = head;
+    while (n--) {
+        for (int i = 1; i < k; i++) {
             pre = now;
             now = now->link;
         }
-        printf("%d",now->data);
-        now = delete(pre,now);
-        if (cnt < n - 1) {printf(", ");}
+        printf("%d%s", now->data, n ? ", " : "");
+
+        pre->link = now->link;
+        free(now);
+        now = pre->link;
     }
     printf(">");
     return 0;
--- week4/1406 에디터.c
+++ week4/1406 에디터.c
@@ -10,77 +10,51 @@ typedef struct node {
 
 void append(char d, node* now) {
     node* new = (node*)malloc(sizeof(node));
-    node* pre = now->left;
-
-    new->right = now;
-    new->left = pre;
     new->data = d;
-
-    pre->right = new;
+    
+    new->right = now;
+    new->left = now->left;
+    
+    now->left->right = new;
     now->left = new;
 }
 
 node* delete(node* now) {
-    if (now->left == NULL || now->right == NULL) {
-        return now;  // 예외 처리: 잘못된 노드 삭제 방지
-    }
-
     node* pre = now->left;
-    node* next = now->right;
-
-    pre->right = next;
-    next->left = pre;
-
-    free(now);
-    return next;
+    pre->left->right = now;
+    now->left = pre->left;
+    
+    free(pre);
+    return now;
 }
 
 void print_list(node* head) {
-    node* tmp = head->right;
-    while (tmp->right != NULL) {
+    for (node* tmp = head->right; tmp->right; tmp = tmp->right)
         putchar(tmp->data);
-        tmp = tmp->right;
-    }
-    printf("\n");
 }
 
 int main() {
-    // 초기 이중 연결 리스트
-    node* head = (node*)malloc(sizeof(node));
-    node* tail = (node*)malloc(sizeof(node));
-    head->right = tail;
-    tail->left = head;
+    node *head = (node*)malloc(sizeof(node)), *tail = (node*)malloc(sizeof(node));
+    head->right = tail, tail->left = head;
     head->left = tail->right = NULL;
 
-    // 리스트 입력
     char tmp[100000];
     scanf("%s", tmp);
 
-    node* now = tail;
-    int len = strlen(tmp);
-    for (int i = len - 1; i >= 0; i--) {
-        append(tmp[i], now);
-        now = now->left;
-    }
-
-    // 명령어 개수
+    for (int i = 0; tmp[i]; i++) 
+        append(tmp[i], tail);
+    
     int m;
     scanf("%d", &m);
 
-    // 명령어 처리
     node* cursor = tail;
-    for (int i = 0; i < m; i++) {
-        char cmd;
+    while (m--) {
+        char cmd, x;
         scanf(" %c", &cmd);
-
-        if (cmd == 'L') {
-            if (cursor->left != head) {cursor = cursor->left;}
-        } else if (cmd == 'D') {
-            if (cursor != tail) {cursor = cursor->right;}
-        } else if (cmd == 'B') {
-            if (cursor->left != head) { cursor = delete(cursor->left);}
-        } else if (cmd == 'P') {
-            char x;
+        if (cmd == 'L' && cursor->left != head) cursor = cursor->left;
+        else if (cmd == 'D' && cursor != tail) cursor = cursor->right;
+        else if (cmd == 'B'&& cursor->left != head) cursor = delete(cursor);
+        else if (cmd == 'P') {
             scanf(" %c", &x);
             append(x, cursor);
         }
