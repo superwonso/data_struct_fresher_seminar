b60b35558d310b244484bcd98bc152ada1cebb10
yuvnn <yubin11890@gmail.com>
2025-03-18 12:46:11 +0900

    week6

---
+++ week6/9372 상근이의 여행.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int t,m,n,a,b;
+    while(t--){
+        scanf("%d %d",n,m);
+        while(m--){
+            scanf("%d %d",a,b);
+        }
+        printf("%d\n", n - 1); 
+    }
+    return 0;
+}
+
